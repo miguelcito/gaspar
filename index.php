0c55da2520e1ca92d57a300900ea85a981dbf5d1
miguel <espinoza@12>
2016-03-03 22:44:31 -0500

    gaspar y mily amigas

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>mily y graciela</title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+<h>hola pe  amia  mily </h>
+<<img src="graciela.jpe" alt="">
+<h>hola amita  graciel a como estas amia </h>
+<<img src="milagros.jpe" alt="">
+<h>yo todo bien gracias y usted</h>	
+</body>
+</html>
